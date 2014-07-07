d1804a5bdc27e68b8d3771c35a9dfaf36a672bcb
Mathis Bareis <mathis.bareis@tum.de>
2014-07-07 11:00:32 +0200

    Added a check to make sure every email address is unique.

--- src/de/tum/asp/sharenow/app/RegisterActivity.java
+++ src/de/tum/asp/sharenow/app/RegisterActivity.java
@@ -4,6 +4,8 @@ import de.tum.asp.sharenow.R;
 import de.tum.asp.sharenow.database.LocalDatabase;
 import de.tum.asp.sharenow.util.User;
 import android.support.v7.app.ActionBarActivity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -37,28 +39,44 @@ public class RegisterActivity extends ActionBarActivity {
 
 		// Daten aus View auslesen und in Datenbank speichern
 		LocalDatabase database = new LocalDatabase(getApplicationContext());
-		User user = new User();
 		EditText mail = (EditText) findViewById(R.id.register_mail_input);
-		user.setMail(mail.getText().toString());
-		EditText firstName = (EditText) findViewById(R.id.register_firstName_input);
-		user.setFirstName(firstName.getText().toString());
-		EditText lastName = (EditText) findViewById(R.id.register_lastName_input);
-		user.setLastName(lastName.getText().toString());
-		// Passwort zunächst hashen
-		EditText pw = (EditText) findViewById(R.id.register_password_input);
-		String hashedPassword = SessionManager.hashPassword(pw.getText()
-				.toString());
-		user.setHashedPassword(hashedPassword);
-		database.insert(user);
+		User user = database.getUser(mail.getText().toString());
+		if (user == null) {
+			// E-Mail Adresse noch nicht verwendet
+			user = new User();
+			user.setMail(mail.getText().toString());
+			EditText firstName = (EditText) findViewById(R.id.register_firstName_input);
+			user.setFirstName(firstName.getText().toString());
+			EditText lastName = (EditText) findViewById(R.id.register_lastName_input);
+			user.setLastName(lastName.getText().toString());
+			// Passwort zunächst hashen
+			EditText pw = (EditText) findViewById(R.id.register_password_input);
+			String hashedPassword = SessionManager.hashPassword(pw.getText()
+					.toString());
+			user.setHashedPassword(hashedPassword);
+			database.insert(user);
 
-		// Login durchführen
-		SessionManager sessionManager = new SessionManager(
-				getApplicationContext());
-		sessionManager.login(user.getMail(), pw.getText().toString());
+			// Login durchführen
+			SessionManager sessionManager = new SessionManager(
+					getApplicationContext());
+			sessionManager.login(user.getMail(), pw.getText().toString());
 
-		// nach erfolgreicher Registrierung zurück zu Haupt-Aktivität wechseln
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
+			// nach erfolgreicher Registrierung zurück zu Haupt-Aktivität
+			// wechseln
+			Intent intent = new Intent(this, MainActivity.class);
+			startActivity(intent);
+		} else {
+			// E-Mail Adresse bereits verwendet.
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setMessage(R.string.register_popup_text);
+			builder.setPositiveButton(R.string.register_popup_ok,
+					new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int id) {
+						}
+					});
+			AlertDialog dialog = builder.create();
+			dialog.show();
+		}
 	}
 
 }
